1d93678b1a95022d5f2a27ce52cd6beb07735404
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-07 21:26:14 +0000

    Add project files.

---
+++ TryCatchExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20, num2 = 5, num3 = 10;
+            int count = 0;
+            double average;
+            //Calculate the average without the try/catch
+            //average = (num1 + num2 + num3) / count;
+
+            try
+            {
+                average = (num1 + num2 + num3) / count;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.Out.WriteLine("the program is attempting to divide by Zero");
+                Console.ReadLine(); 
+            }
+            
+        }
+    }
+}
